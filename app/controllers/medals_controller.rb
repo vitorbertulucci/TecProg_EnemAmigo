b0f0c9db9ad0b012097dca6b57b6ccb0640c6d0d
Vitor Borges <vitorbertulucci@hotmail.com>
2016-09-20 22:52:41 -0300

    Added some techniques on medals controller.

--- app/controllers/medals_controller.rb
+++ app/controllers/medals_controller.rb
@@ -1,10 +1,25 @@
+# Class: medals_controller.rb.
+# Purpose: This class is designed to control the medals Class
+# Enem Amigo.
+# FGA - Universidade de Brasíilia UnB.
+
 class MedalsController < ApplicationController
 
   before_action :authenticate_user
 
+  # Name: index
+  # Objective: show all medals that the current have and all that it doesn't have
+  # Parameters: all medals
+  # Return: a missing medals object
+
   def index
+
     check_medals
     @missing_medals = @medals - current_user.medals
+
+    return @missing_medals
+    return @medals
+
   end
 
 end
