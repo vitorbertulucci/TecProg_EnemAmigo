4abd784cd5db11e8088b15531daf6a570e6c8900
Vitor Borges <vitorbertulucci@hotmail.com>
2016-11-08 09:36:03 -0200

    Identation and correct 'JavaDoc' for rails. Language pattern. Correct error treatment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,163 +5,191 @@
 
 class CommentsController < ApplicationController
 
-  # Checks for user logged in.
-  before_action :authenticate_user
-  before_action :verify_user_permission, only: [:destroy, :edit]
+    # Checks for user logged in.
+    before_action :authenticate_user
+    before_action :verify_user_permission, only: [:destroy, :edit]
 
-  # Name: new
-	# Objective: this method create a new instance of comment on system.
-	# Parameters: don't have parameters.
-	# Return: comment object.
+    # Name:
+	# 	- new
+	# Objective:
+	# 	- this method create a new instance of comment on system.
+	# *	*Args* :
+	#  	- none.
+	# * *Returns* :
+	# 	- +comment+ -> comment object.
 
-  def new
+    def new
 
-    # To instance a new comment object
-    @comment = Comment.new(nil)
-    assert(@comment.kind_of?(Comment), 'The object @comment it could not be instantiated')
+        # To instance a new comment object
+        @comment = Comment.new(nil)
+        assert(@comment.kind_of?(Comment), 'The object @comment it could not be instantiated')
 
-    return @comment
+        return @comment
 
-  end
-
-  # Name: create
-	# Objective: this method create a new instance of company on system.
-	# Parameters: post identifier and topic identifier..
-	# Return: redirect to the topic page or comment page.
-
-  def create
-
-    # To create a new comment on topic.
-    @comment = Comment.new(set_comment_params)
-    assert(@comment.kind_of?(Comment), 'The object @comment it could not be instantiated'
-    + 'because does not belong to controller')
-
-    # Add the attribute id of current user to create a new comment object
-    @comment.user_id = current_user.id
-    assert(@comment.user_id != nil, 'The attribute user_id of @comment is null')
-
-    # Add the attribute post id to create a new comment object
-    @comment.post_id = params[:post_id]
-    assert(@comment.post_id != nil, 'The attribute post_id of @comment is null')
-
-    # Validate the creation of the object to redirect their respective topic
-    if (@comment.save)
-      flash[:success] = 'Seu comentário foi criado com sucesso'
-      return redirect_to(Topic.find(session[:topic_id]))
-    else
-      return redirect_to(new_post_comment_path(params[:post_id]))
     end
 
-  end
+    # Name:
+	# 	- create
+	# Objective:
+	# 	- this method create a new instance of company on system.
+	# *	*Args* :
+	#  	- post identifier and topic identifier..
+	# * *Returns* :
+	# 	- +redirect+ -> redirect to the topic page or comment page.
 
-  # Name: edit.
-	# Objective: this method find the comment to be edited or deleted.
-	# Parameters: don't have parameters.
-	# Return: comment object.
+    def create
 
-  def edit
+        # To create a new comment on topic.
+        @comment = Comment.new(set_comment_params)
+        assert(@comment.kind_of?(Comment), 'The object @comment it could not be instantiated'
+        + 'because does not belong to controller')
 
-    # To seek proper comment to be edited using your id as parameter
-    @comment = Comment.find(params[:comment_id])
-    assert(@comment != nil, 'The object @comment is null')
+        # Add the attribute id of current user to create a new comment object
+        @comment.user_id = current_user.id
+        assert(@comment.user_id != nil, 'The attribute user_id of @comment is null')
 
-    return @comment
+        # Add the attribute post id to create a new comment object
+        @comment.post_id = params[:post_id]
+        assert(@comment.post_id != nil, 'The attribute post_id of @comment is null')
 
-  end
+        # Validate the creation of the object to redirect their respective topic
+        if (@comment.save)
+          flash[:success] = 'Seu comentário foi criado com sucesso'
+          return redirect_to(Topic.find(session[:topic_id]))
+        else
+          return redirect_to(new_post_comment_path(params[:post_id]))
+        end
 
-  # Name: update.
-  # Objective: this method edit an comment in the database.
-	# Parameters: comment identifier and topic identifier.
-	# Return: redirect to topic page or edited post.
-
-  def update
-
-    # To update the review with their modifications due, taking the function to edit attributes
-    @comment = Comment.find(params[:comment_id])
-    assert(@comment != nil, 'The object @comment is null')
-
-    # Validate the update of the object to redirect their respective topic
-    if (@comment.update_attributes(comment_params))
-      flash[:success] = "Seu comentário foi atualizado com sucesso"
-      return redirect_to(Topic.find(session[:topic_id]))
-    else
-      return redirect_to(edit_post_comment_path(session[:topic_id]))
     end
 
-  end
+    # Name:
+	# 	- edit
+	# Objective:
+	# 	- this method find the comment to be edited or deleted.
+	# *	*Args* :
+	#  	- none.
+	# * *Returns* :
+	# 	- +comment+ -> comment object
 
-  # Name: rate_comment
-	# Objective: this method rate a comment in topic page.
-	# Parameters: comment identifier.
-	# Return: save comment.
+    def edit
 
-  def rate_comment
+        # To seek proper comment to be edited using your id as parameter
+        @comment = Comment.find(params[:comment_id])
+        assert(@comment != nil, 'The object @comment is null')
 
-    render nothing: true
+        return @comment
 
-    # Destined to find its comment and make a request to rate.
-    comment = Comment.find(params[:id])
-    assert(@comment != nil, 'The object @comment is null')
-
-    # Validate the evaluations made by the current user on the system and then save.
-    if(!comment.user_ratings.include? current_user.id)
-      comment.user_ratings.push(current_user.id)
-      comment.save
-    else
-      return redirect_to_back(root_path)
     end
 
-  end
+    # Name:
+	# 	- update
+	# Objective:
+	# 	- this method edit an comment in the database.
+	# *	*Args* :
+	#  	- comment identifier and topic identifier..
+	# * *Returns* :
+	# 	- +redirect+ -> redirection to Topic page or edit comment page
 
-  # Name: destroy
-	# Objective: this method delete an commment on database .
-	# Parameters: comment identifier.
-	# Return: redirect the user to topic page.
+    def update
 
-  def destroy
+        # To update the review with their modifications due, taking the function to edit attributes
+        @comment = Comment.find(params[:comment_id])
+        assert(@comment != nil, 'The object @comment is null')
 
-    # Destined to find its comment and make a request to delete.
-    @comment = Comment.find(params[:comment_id])
-    assert(@comment != nil, 'The object @comment is null')
+        # Validate the update of the object to redirect their respective topic
+        if (@comment.update_attributes(comment_params))
+          flash[:success] = "Seu comentário foi atualizado com sucesso"
+          return redirect_to(Topic.find(session[:topic_id]))
+        else
+          return redirect_to(edit_post_comment_path(session[:topic_id]))
+        end
 
-    # To delete the comment, destroying their dependencies with topic foreign key.
-    @comment.destroy
-    assert(@comment == nil, 'The object @comment was not destroyed because'
-    + 'isnt null')
+    end
 
-    # Show on the screen a success message in operation.
-    flash[:success] = "Comentário deletado com sucesso"
+    # Name:
+	# 	- rate_comment
+	# Objective:
+	# 	- this method rate a comment in topic page.
+	# *	*Args* :
+	#  	- comment identifier.
+	# * *Returns* :
+	# 	- none.
 
-    return redirect_to(Topic.find(session[:topic_id]))
+    def rate_comment
 
-  end
+        render nothing: true
 
-  # Name: show.
-	# Objective: this method renders the comment's page.
-	# Parameters: comment identifier.
-	# Return: comment object.
+        # Destined to find its comment and make a request to rate.
+        comment = Comment.find(params[:id])
+        assert(@comment != nil, 'The object @comment is null')
 
-  def show
+        # Validate the evaluations made by the current user on the system and then save.
+        if(!comment.user_ratings.include? current_user.id)
+            comment.user_ratings.push(current_user.id)
+            begin
+                comment.save
+            rescue Exception
+                flash[:error] = "Não foi possível realizar o comentário. Tente novamente!"
+            end
+        else
+            return redirect_to_back(root_path)
+        end
 
-    # Find the commentar for viewing.
-    @comment = Comment.find(params[:id])
-    assert(@comment != nil, 'The object @comment is null')
+    end
 
-    return @comment
+    # Name:
+	# 	- destroy
+	# Objective:
+	# 	- this method delete an commment on database .
+	# *	*Args* :
+	#  	- comment identifier
+	# * *Returns* :
+	# 	- +redirect+ -> redirection to topic page
 
-  end
+    def destroy
 
-  private
+        # Destined to find its comment and make a request to delete.
+        @comment = Comment.find(params[:comment_id])
+        assert(@comment != nil, 'The object @comment is null')
 
-  # Name: set_comment_params
-	# Objective: this method leads the comment's parameters for the methods.
-	# Parameters: company object.
-	# Return: don't have return.
+        # To delete the comment, destroying their dependencies with topic foreign key.
+        @comment.destroy
+        assert(@comment == nil, 'The object @comment was not destroyed because'
+        + 'isnt null')
 
-  def set_comment_params
+        # Show on the screen a success message in operation.
+        flash[:success] = "Comentário deletado com sucesso"
 
-    params.require(:comment).permit(:content)
+        return redirect_to(Topic.find(session[:topic_id]))
 
-  end
+    end
+
+    # Name:
+	# 	- show
+	# Objective:
+	# 	- this method renders the comment's page.
+	# *	*Args* :
+	#  	- comment identifier
+	# * *Returns* :
+	# 	- +comment+ -> comment object
+
+    def show
+
+        # Find the commentar for viewing.
+        @comment = Comment.find(params[:id])
+        assert(@comment != nil, 'The object @comment is null')
+
+        return @comment
+
+    end
+
+    private
+
+
+        def set_comment_params
+
+            params.require(:comment).permit(:content)
+
+        end
 
 end
